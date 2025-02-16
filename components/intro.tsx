a474e553cebc5d1b4eccb7edf5b28307a2103394
erik-ksth <erikhein.ksth@gmail.com>
2025-02-15 22:14:41 -0800

    update: resume

--- components/intro.tsx
+++ components/intro.tsx
@@ -86,7 +86,7 @@ export default function Intro() {
         </Link>
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full cursor-pointer outline-none focus:scale-110 hover:scale-110 active:scale-105 transition borderBlack dark:bg-white/10"
-          href="/Erik_Hein_resume.pdf"
+          href="/Erik Hein Resume.pdf"
           target="_blank"
         >
           View Resume{" "}
